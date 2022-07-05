c89309ff8925bd8f0f36a0609287efd5f260fe0f
ivancampo08 <iheanyichukwukalu88@gmail.com>
2022-07-04 21:37:03 -0700

    variables if else while task

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,13 +12,12 @@ int main(void)
 	 * 0 1 2 3 4 5 6 7 8 9 a b c d e f
 	 */
 	int n;
+	char ch;
 
 	for (n = 48; n <= 57; n++)
 	{
 		putchar(ch);
 	}
-	char ch;
-
 	for (ch = 'a'; ch <= 'f'; ch++)
 	{
 		putchar(10);
