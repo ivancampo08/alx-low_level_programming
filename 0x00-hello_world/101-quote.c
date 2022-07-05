bca76763587ebf153eae48404f0c857321d4f4cb
ivancampo08 <iheanyichukwukalu88@gmail.com>
2022-07-04 20:15:57 -0700

    hello world task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,11 +5,10 @@
 * main - prints exactly "and that place of art is useful"
 * - Dora Korper, 2015-10-19",
 * followed by a new line, to the standard error.
-* Return: Always 0 (Success)
+* Return: 0 if exited properly, non-zero otherwise
 */
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 59);
-char (10);
 return (1);
 }
