13f70ae427806a586f5cdd37456e91bfbce6305f
ivancampo08 <iheanyichukwukalu88@gmail.com>
2022-07-07 21:28:49 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,17 +2,19 @@
 
 /**
 * print_line -> printing line
+* @n: integer params
 */
+
 void print_line(int n)
 {
-int x;
+	int x;
 
-if (n <= 0)
-_putchar('\n');
-else
-{
-for (x = 0; x < n; x++)
-_putchar ('95');
-_putchar('\n');
-}
+	if (n <= 0)
+	_putchar('\n');
+	else
+	{
+	for (x = 0; x < n; x++)
+	_putchar ('95');
+	_putchar('\n');
+	}
 }
