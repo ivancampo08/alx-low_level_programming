ac404052347b86a39997d0b576a5d932bd2b74df
ivancampo08 <iheanyichukwukalu88@gmail.com>
2022-07-11 18:36:24 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-char str1 || = "and that piece of art is useful\" - Dora Korper, 2015-10-19\n";
-write(2, str1, 59);
+write(2, "and that piece of art is useful\" - Dora Korper, 2015-10-19");
+write("\n", 59);
 return (1);
 }
