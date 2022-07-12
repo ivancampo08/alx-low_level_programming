a312910bcdcc44c839a4aad2c08b8175ba37a523
ivancampo08 <iheanyichukwukalu88@gmail.com>
2022-07-11 18:49:57 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,9 @@
 #include <unistd.h>
 
 /**
-* main - Entry point
+* main - prints exactly "and that place of art is useful"
+* - Dora Korper, 2015-10-19",
+*   followed by a new lne, to the standard error.
 * Reture: Always 1 (Success)
 */
 
