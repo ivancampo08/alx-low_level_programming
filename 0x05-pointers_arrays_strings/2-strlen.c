a902eda3ede64dae6e18565a80fcc479f45eba2e
ivancampo08 <iheanyichukwukalu88@gmail.com>
2022-07-12 14:15:40 -0700

    length of string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 /**
-* _strlen -> functionto get the length of a string
+* _strlen -> function to get the length of a string
 * @s: string pointer to passed to this function
 * Return: returns length of the string
 */
