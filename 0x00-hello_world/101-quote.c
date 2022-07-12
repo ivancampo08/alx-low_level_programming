10eebafb0b287ecab431714bddbbb9d1828f62b1
ivancampo08 <iheanyichukwukalu88@gmail.com>
2022-07-11 18:55:37 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
 * main - prints exactly "and that place of art is useful"
 * - Dora Korper, 2015-10-19",
 *   followed by a new lne, to the standard error.
-* Reture: Always 1 (Success)
+* Reture: 1 if exited properly, otherwise 0
 */
 
 int main(void)
