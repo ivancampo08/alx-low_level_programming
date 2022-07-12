0b0ae33f14f83dbcb144f2f137086a7cb266b628
ivancampo08 <iheanyichukwukalu88@gmail.com>
2022-07-12 11:37:15 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,13 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
-* main - prints exactly "and that place of art is useful"
-* - Dora Korper, 2015-10-19",
-*   followed by a new lne, to the standard error.
-* Reture: 1 if exited properly, otherwise 0
+* main - Entry point
+* Reture: Always 1 (Success)
 */
 
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korper, 2015-10-19);
-(\n", 59);
+char str1[] = "and that piece of art is useful\" -Dora Korper, 2015-10-19\n";
+write(2, str1, 59);
 return (1);
 }
