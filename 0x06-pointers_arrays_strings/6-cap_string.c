8360e5892dfe9c029398141795a60a85ec26bf0f
ivancampo08 <iheanyichukwukalu88@gmail.com>
2022-07-15 15:08:06 -0700

    funtions of string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,14 +13,14 @@ char *cap_string(char *x)
 
 	while (x[a])
 	{
-		i = 0;
-		while (i < len)
-		{
-			if ((a == 0 || x[a - 1] == spc[i]) && (x[a] >= 97 && x[a] <= 22))
-			x[a] = x[a] - 32;
-			i++
-		}
-		a++;
+	i = 0;
+	while (i < len)
+	{
+	if ((a == 0 || x[a - 1] == spc[i]) && (x[a] >= 97 && x[a] <= 22))
+	x[a] = x[a] - 32;
+	i++
+	}
+	a++;
 	}
 	return (x);
 }
