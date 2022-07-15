185a6888737a79ce1cae98af90b951b82a66636b
ivancampo08 <iheanyichukwukalu88@gmail.com>
2022-07-15 10:28:03 -0700

    concatenating two strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _strncat -> function to append some charcters
+* @dest: first param
+* @src: second param
+* @n: third param
+* Return: string
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n)
+		return (dest);
+}
