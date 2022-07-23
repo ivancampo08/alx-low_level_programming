16a1f6522f72d0789f30fd051304079c5e644c44
ivancampo08 <iheanyichukwukalu88@gmail.com>
2022-07-22 19:19:30 -0700

    argc and argv task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,16 +13,16 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		for (j =0; argv[i][j] != '\0'; j++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (!isdigit(argv[i][j]))
 			{
-				print("Error\n");
+				printf("Error\n");
 				return (1);
 			}
 		}
-		add += atoi argv[i]);
-		printf("%d\n", add);
-		return (0)
+		add += atoi(argv[i]);
 	}
+		printf("%d\n", add);
+		return (0);
 }
