0155491a26e46650a955910808a692a4328416fd
ivancampo08 <iheanyichukwukalu88@gmail.com>
2022-07-22 19:45:56 -0700

    prints minimum number

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -16,12 +16,12 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	cents = atio(argv[1]);
+	cents = atoi(argv[1]);
 
 	while (cents > 0)
 	{
 		if (cents >= 25)
-			cents -= 25;
+		cents -= 25;
 		else if (cents >= 10)
 			cents -= 10;
 		else if (cents >= 5)
@@ -32,6 +32,6 @@ int main(int argc, char *argv[])
 			cents -= 1;
 		ncoins += 1;
 	}
-	printf("d\n", ncoins);
+	printf("%d\n", ncoins);
 	return (0);
 }
