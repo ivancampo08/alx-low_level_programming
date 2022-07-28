ea1dfa0908e79fcbec1e52e001100f8f4810d996
ivancampo08 <iheanyichukwukalu88@gmail.com>
2022-07-28 13:28:06 -0700

    string concatenat

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* malloc_checked - checking malloc
+* @b: param b
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+this is a more malloc task readme file
