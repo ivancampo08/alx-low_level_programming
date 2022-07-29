8715a6b31b505ac623d3e97580b1d8a3d9415d5f
ivancampo08 <iheanyichukwukalu88@gmail.com>
2022-07-29 16:19:05 -0700

    checked malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,8 +4,17 @@
 /**
 * malloc_checked - checking malloc
 * @b: param b
+*
+* Return: pointer to the allocated memory
+* ifmalloc fails, status value is equal to 98
 */
 void *malloc_checked(unsigned int b)
 {
-	void *p;
+	char *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
 
