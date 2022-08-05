a8bffa425c7584acfa5747855d13a262183e4e11
ivancampo08 <iheanyichukwukalu88@gmail.com>
2022-08-04 21:32:28 -0700

    hot fix

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,7 +9,7 @@
 void print_all(const char * const format, ...)
 {
 	va_list valist;
-	unsigned int i = 0, j, c = 0;
+	unsigned int i = 0, j = 0, c = 0;
 	char *str;
 	const char t_arg[] = "cifs";
 
@@ -25,7 +25,7 @@ void print_all(const char * const format, ...)
 				break;
 			} j++;
 		}
-		swtich (format[i])
+		swtich(format[i])
 		{
 		case 'c':
 			printf("%c", va_arg(valist, int)), c = 1;
@@ -47,5 +47,6 @@ void print_all(const char * const format, ...)
 			break;
 		} i++;
 	}
-	printf("\n"), va_end(valist);
+	printf("\n");
+	va_end(valist);
 }
