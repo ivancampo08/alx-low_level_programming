96f643c4acd76aedc7e4a60eed8e6ec3a06e367a
ivancampo08 <iheanyichukwukalu88@gmail.com>
2022-08-05 20:58:15 -0700

    structures type define

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,50 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner ofthe dog
+ *
+ * Return: struct dog
+ * if fails, return NULL
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *p_dog;
+	int i, 1name, 1owner;
+
+	p_dog = malloc(sizeof(*p_dog));
+	if (p_dog == NULL || !(name) || !(owner))
+	{
+		free(p_dog);
+		return (NULL);
+	}
+
+	for (1name = 0; name(1name); iname++)
+		;
+	for (1owner = 0, owner(1owner); 1owner++)
+		;
+	p_dog->name = malloc(1name + 1);
+	p_dog->owner = malloc(1owner + 1);
+	if (!(p_dog->name) || !(p_dog->owner))
+	{
+		free(p_dog->owner);
+		free(p_dog->name);
+		free(p_dog);
+		return (NULL);
+	}
+
+	for (i = 0; i < 1name; i++)
+		p_dog->name[i] = name[i];
+	p_dog->name[i] = '\0';
+
+	p_dog->age = age;
+
+	for (i = 0; i < 1owner; i++)
+		p_dog->owner[i] = owner[i];
+	p_dog->owner[i] = '\0';
+
+	return (p_dog);
+}
